80ed73d52a6b26261c128606a2388383a80703eb
0JIEUN0 <jieun4436@naver.com>
2021-05-05 22:34:28 +0900

    Tab Bar: using ChipNavigationBar

--- app/src/main/java/com/mp/vocabulary/MainActivity.kt
+++ app/src/main/java/com/mp/vocabulary/MainActivity.kt
@@ -2,10 +2,40 @@ package com.mp.vocabulary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import com.mp.vocabulary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        init()
+    }
+
+    private fun init() {
+        binding.menuBottom.setOnItemSelectedListener { id ->
+            var fragment: Fragment? = null
+            when(id) {
+                R.id.searchMenu -> fragment = SearchFragment()
+                R.id.quizMenu -> fragment = QuizFragment()
+                R.id.transMenu -> fragment = TranslationFragment()
+                R.id.myPageMenu -> fragment = MypageFragment()
+            }
+            fragment?.let {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, fragment)
+                    .commit()
+            } ?: Log.d("ERROR", "Error:: can't make new fragment")
+        }
+
+        binding.menuBottom.setItemSelected(R.id.searchMenu, true)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.container, SearchFragment())
+            .commit()
+
     }
 }
