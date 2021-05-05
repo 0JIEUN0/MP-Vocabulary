c5425d38a92e84827444f39933843ca6e7305730
0JIEUN0 <jieun4436@naver.com>
2021-05-05 20:28:18 +0900

    init fragments (search, quiz, translation, mypage)

---
+++ app/src/main/java/com/mp/vocabulary/QuizFragment.kt
@@ -0,0 +1,26 @@
+package com.mp.vocabulary
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mp.vocabulary.databinding.FragmentQuizBinding
+
+class QuizFragment : Fragment() {
+    var binding: FragmentQuizBinding? = null
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentQuizBinding.inflate(layoutInflater, container, false)
+        return binding!!.root
+    }
+}
---
+++ app/src/main/java/com/mp/vocabulary/TranslationFragment.kt
@@ -0,0 +1,26 @@
+package com.mp.vocabulary
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mp.vocabulary.databinding.FragmentTranslationBinding
+
+class TranslationFragment : Fragment() {
+    var binding: FragmentTranslationBinding? = null
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentTranslationBinding.inflate(layoutInflater, container, false)
+        return binding!!.root
+    }
+}
