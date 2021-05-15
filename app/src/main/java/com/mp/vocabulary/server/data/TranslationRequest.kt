73b6d76acf48c9a7962d28c096e783cbc4d8b3bc
0JIEUN0 <jieun4436@naver.com>
2021-05-15 11:37:38 +0900

    Add & Change Translation Request/Response

---
+++ app/src/main/java/com/mp/vocabulary/server/data/TranslationRequest.kt
@@ -0,0 +1,7 @@
+package com.mp.vocabulary.server.data
+
+class TranslationRequest (
+    var source: String,
+    var target: String,
+    var text: String
+)
--- app/src/main/java/com/mp/vocabulary/server/data/TranslationResponse.kt
+++ app/src/main/java/com/mp/vocabulary/server/data/TranslationResponse.kt
@@ -1,10 +1,13 @@
 package com.mp.vocabulary.server.data
 
-class TranslationResponse (
-    var srcLangType: String,
-    var tarLangType: String,
-    var translatedText: String
-    )
+class TranslationResponse (var message: TranslationResultResponse)
+class TranslationResultResponse(var result: TranslationResult)
+
+class TranslationResult (
+        var srcLangType: String,
+        var tarLangType: String,
+        var translatedText: String
+        )
 
 enum class CODE {
     ko, en
